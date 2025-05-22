30597ac133d4d1f2602e39033ec5b470ea90f1d3
Kitto03ng <nicolas.gutierrez6@mail.udp.cl>
2025-05-22 15:18:24 -0400

    Update GameGestion.java

--- LAB3 EDA/src/GameGestion.java
+++ LAB3 EDA/src/GameGestion.java
@@ -262,6 +262,6 @@ public class GameGestion {
         }
     }
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("diavlo");
     }
 }
